c9e3b2d36f48cf25c440dc7350514ed359b0290e
Dhruvi Makati <dhruvi.m@simformsolutions.com>
2022-03-14 12:03:12 +0530

    Changes done..!

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,38 +1,38 @@
 # for input comparision
-Number_Dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+number_dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 # TO get LCM
-def Get_LCM(Number1, Number2):
-   if Number1 > Number2:
-       Greater = Number1
+def get_lcm(number1, number2):
+   if number1 > number2:
+       greater = number1
    else:
-       Greater = Number2
+       greater = number2
    while True:
-       if (Greater % Number1 == 0) and (Greater % Number2 == 0):
-           LCM = Greater
+       if (greater % number1 == 0) and (greater % number2 == 0):
+           LCM = greater
            break
-       Greater += 1
+       greater += 1
    return int(LCM)
 
 # To get GCD
-def Get_GCD(Numerator, Denominator):
-    GCD = str(int(Numerator / Denominator))
-    Number_Dictionary_Count = 0
-    while Number_Dictionary_Count < len(Number_Dictionary):
-        GCD = GCD.replace(str(Number_Dictionary_Count), Number_Dictionary[Number_Dictionary_Count])
-        Number_Dictionary_Count += 1
+def get_gcd(numerator, denominator):
+    GCD = str(int(numerator / denominator))
+    number_dictionary_count = 0
+    while number_dictionary_count < len(number_dictionary):
+        GCD = GCD.replace(str(number_dictionary_count), number_dictionary[number_dictionary_count])
+        number_dictionary_count += 1
     return GCD
 
 # To get input from user
-Number1 = input("Enter First Number:").replace(" ", "")
-Number2 = input("Enter Second Number:").replace(" ", "")
-Number_Dictionary_Count = 0
-while Number_Dictionary_Count < len(Number_Dictionary):
-    Number1 = Number1.replace(Number_Dictionary[Number_Dictionary_Count], str(Number_Dictionary_Count))
-    Number2 = Number2.replace(Number_Dictionary[Number_Dictionary_Count], str(Number_Dictionary_Count))
-    Number_Dictionary_Count += 1
-Number1 = int(Number1)
-Number2 = int(Number2)
+number1 = input("Enter First Number:").replace(" ", "")
+number2 = input("Enter Second Number:").replace(" ", "")
+number_dictionary_count = 0
+while number_dictionary_count < len(number_dictionary):
+    number1 = number1.replace(number_dictionary[number_dictionary_count], str(number_dictionary_count))
+    number2 = number2.replace(number_dictionary[number_dictionary_count], str(number_dictionary_count))
+    number_dictionary_count += 1
+number1 = int(number1)
+number2 = int(number2)
 
 # To get final result
-print(Get_GCD(Number1 * Number2, (Get_LCM(Number1, Number2))))
+print(get_gcd(number1 * number2, (get_lcm(number1, number2))))
--- Assignment-2.py
+++ Assignment-2.py
@@ -1,23 +1,23 @@
 # To get combinations of parenthesis and print it out
-def printParenthesis(Str, Position, Count, Open, Close):
-	if Close == Count :
-		Parenthesis = "'"
-		for i in Str:
-			Parenthesis += i
-		print(Parenthesis, end = "', ")
+def print_parenthesis(str, position, count, open, close):
+	if close == count :
+		parenthesis = "'"
+		for i in str:
+			parenthesis += i
+		print(parenthesis, end = "', ")
 		return
 	else:
-		if Open > Close :
-			Str[Position] = ')'
-			printParenthesis(Str, Position + 1, Count, Open, Close + 1)
-		if Open < Count :
-			Str[Position] = '('
-			printParenthesis(Str, Position + 1, Count, Open + 1, Close)
+		if open > close :
+			str[position] = ')'
+			print_parenthesis(str, position + 1, count, open, close + 1)
+		if open < count :
+			str[position] = '('
+			print_parenthesis(str, position + 1, count, open + 1, close)
 			
 # To get input from user
-Count = int(input("Enter number of combinations required: "))
-if Count > -1 and Count <= 8 :
-    Str = [""] * 2 * Count
-    printParenthesis(Str, 0, Count, 0, 0)
+count = int(input("Enter number of combinations required: "))
+if count > -1 and count <= 8 :
+    str = [""] * 2 * count
+    print_parenthesis(str, 0, count, 0, 0)
 else:
     print("Enter number between -1 to 8")
